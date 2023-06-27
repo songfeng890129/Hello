46b215abec1c599fb752977e162b99e841f693ad
bie <luoyu_bie@sina.com>
2023-06-27 13:32:31 +0800

    创建分支

--- Maven_Web_01/Maven_Web_01/src/main/java/com/songfeng/Hello.java
+++ Maven_Web_01/Maven_Web_01/src/main/java/com/songfeng/Hello.java
@@ -3,5 +3,6 @@ package com.songfeng;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello songfeng");
+        System.out.println("创建分支");
     }
 }
